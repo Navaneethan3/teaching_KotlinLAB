179b583a81aeedbbdd9572e0e184534d9b0ac911
Navaneethan3 <104858289+Navaneethan3@users.noreply.github.com>
2026-02-10 10:42:52 +0530

    Update 11.FunctionOverloading.kt

--- Introduction to programming in Kotlin/11.FunctionOverloading.kt
+++ Introduction to programming in Kotlin/11.FunctionOverloading.kt
@@ -7,7 +7,7 @@ fun main() {
 
 fun getData(a: Int, b: Int) = if (a > b) a else b  // overloading by changing type.
 
-// duplicate line is cont + d,formate line is cont + alt + l, context/action  alt + enter
+// duplicate line is cont + d,formate line is cont + alt + l, context or action  alt + enter
 fun getData(a: Double, b: Double) = if (a > b) a else b
 
 fun getData(a: Double, b: Double, c: Double) = if (a >= b && a >= c) a else if (b >= a && b >= c) b else c
